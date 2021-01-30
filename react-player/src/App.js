087376413518716c0cd9c7590c89f701a7466e57
kevin frias <kevinfrias@ymail.com>
2021-01-30 16:36:34 -0600

    Added play & pause functionality

--- react-player/src/App.js
+++ react-player/src/App.js
@@ -11,11 +11,12 @@ function App() {
   //State 
   const [songs, setSongs] = useState(data());
   const [currentSong, setCurrentSong] = useState(songs[0])
+  const [isPlaying, setIsPlaying] = useState(false);
 
   return (
     <div className="App">
       <Song currentSong={currentSong} />
-      <Player />
+      <Player isPlaying={isPlaying} setIsPlaying={setIsPlaying} currentSong={currentSong} />
     </div>
   );
 }
